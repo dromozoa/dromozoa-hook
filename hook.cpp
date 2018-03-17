d33505ac203ff3f50be8e1e1f2399a59529d0ccf
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-03-17 17:53:08 +0900

    hookを修正。

--- hook.cpp
+++ hook.cpp
@@ -1,19 +1,19 @@
-// Copyright (C) 2016 Tomoyuki Fujimori <moyu@dromozoa.com>
+// Copyright (C) 2016,2018 Tomoyuki Fujimori <moyu@dromozoa.com>
 //
-// This file is part of dromozoa-atexit.
+// This file is part of dromozoa-hook.
 //
-// dromozoa-atexit is free software: you can redistribute it and/or modify
+// dromozoa-hook is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// dromozoa-atexit is distributed in the hope that it will be useful,
+// dromozoa-hook is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with dromozoa-atexit.  If not, see <http://www.gnu.org/licenses/>.
+// along with dromozoa-hook.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <stdlib.h>
 #include <string.h>
@@ -42,9 +42,9 @@ namespace {
   void hook_atexit() {
     char** save = environ;
     try {
-      if (const char* command = getenv("DROMOZOA_HOOK_ATEXIT")) {
-        std::vector<const char*> envs;
-        for (const char* const* i = environ; *i != '\0'; ++i) {
+      if (char* command = getenv("DROMOZOA_HOOK_ATEXIT")) {
+        std::vector<char*> envs;
+        for (char** i = environ; *i != '\0'; ++i) {
           bool unset = false;
           for (const char* const* j = unset_envs; *j; ++j) {
             if (starts_with(*i, *j)) {
@@ -59,9 +59,9 @@ namespace {
         std::ostringstream out;
         out << "DROMOZOA_HOOK_PID=" << getpid();
         std::string pid = out.str();
-        envs.push_back(pid.c_str());
+        envs.push_back(const_cast<char*>(pid.c_str()));
         envs.push_back(0);
-        environ = const_cast<char**>(&envs[0]);
+        environ = &envs[0];
         system(command);
       }
     } catch (...) {}
